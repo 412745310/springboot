c957badf0f007b7191fa11c8171fffa1944c482f
412745310@qq.com <412745310@qq.com>
2017-07-20 01:31:53 +0800

    修改mapperscan注解位置

--- springboot-mybatis/src/main/java/com/chelsea/sprintboot_mybatis/App.java
+++ springboot-mybatis/src/main/java/com/chelsea/sprintboot_mybatis/App.java
@@ -1,9 +1,11 @@
 package com.chelsea.sprintboot_mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chelsea.sprintboot_mybatis.dao")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
--- springboot-mybatis/src/main/java/com/chelsea/sprintboot_mybatis/controller/TestController.java
+++ springboot-mybatis/src/main/java/com/chelsea/sprintboot_mybatis/controller/TestController.java
@@ -2,7 +2,6 @@ package com.chelsea.sprintboot_mybatis.controller;
 
 import java.util.List;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +12,6 @@ import com.chelsea.sprintboot_mybatis.service.TestService;
 
 @RestController
 @RequestMapping("/test")
-@MapperScan("com.chelsea.sprintboot_mybatis.dao")
 public class TestController {
 
 	@Autowired
