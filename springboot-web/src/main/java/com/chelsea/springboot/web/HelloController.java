0632fc4194046948578e70eadae2e8d5520d854d
baojun_a <baojun_a@aspirecn.com>
2018-09-27 15:32:12 +0800

    属性文件键值获取

--- springboot-web/src/main/java/com/chelsea/springboot/web/HelloController.java
+++ springboot-web/src/main/java/com/chelsea/springboot/web/HelloController.java
@@ -1,14 +1,21 @@
 package com.chelsea.springboot.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
+    @Value("${foo}")
+    private String foo;
+    
+    @Value("${age}")
+    private String age;
 	
 	@RequestMapping("/sayHello")
 	public String sayHello(){
-		return "hello world111";
+		return "hello world " + foo + "," + age;
 	}
 
 }
