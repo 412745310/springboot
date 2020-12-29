c26067efbac4622b6826304b4d1faa89fb7d8988
baojun_a <baojun_a@aspirecn.com>
2020-12-29 16:11:47 +0800

    新增kafka批量消费功能

--- springboot-kafka/src/main/java/com/chelsea/springboot/kafka/KafkaConsumer.java
+++ springboot-kafka/src/main/java/com/chelsea/springboot/kafka/KafkaConsumer.java
@@ -1,5 +1,8 @@
 package com.chelsea.springboot.kafka;
 
+import java.util.List;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +15,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = {"testTopic"})
-    public void receive(String message) throws InterruptedException {
-        System.out.println("app_log--消费消息:" + message);
-        Thread.sleep(10000);
+    /**
+     * 监听批量消费
+     * 
+     * @param records
+     */
+    @KafkaListener(topics = "covid19", containerFactory = "batchFactory")
+    public void listen(List<ConsumerRecord<String, String>> records) {
+        records.forEach(record -> consumer(record));
+    }
+
+    /**
+     * 监听单条消费
+     */
+    //@KafkaListener(topics = {"testlog"})
+    public void listen2(ConsumerRecord<String, String> record) {
+        consumer(record);
+    }
+
+    public void consumer(ConsumerRecord<String, String> record) {
+        System.out.println("主题: " + record.topic() + ", 内容: " + record.value());
     }
 
 }
